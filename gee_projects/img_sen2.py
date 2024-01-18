0403e61281990715af5467a14175c2840ef7d42f
reutkeller <reutkeller@gmail.com>
2024-01-18 10:30:21 +0200

    added module for mask s2

--- gee_projects/img_sen2.py
+++ gee_projects/img_sen2.py
@@ -14,6 +14,7 @@ ee.Authenticate()
 ee.Initialize(project='ee-reutkeller')
 
 from . import util_func as utils
+from . import const_vars as const
 
 # %% ../nbs/satellite_data/sentinel_2.ipynb 5
 class access_s2_image():
@@ -21,8 +22,24 @@ class access_s2_image():
   def __init__(self,
                path_to_geometry : str , # path to geometry file, can be .shp or .gpkg . example : r"D:\git\gee_maps\nbs\geometry\test_bbox.gpkg"
                crs : str , #coordinate reference system to work with
+              #  start_date : str , # Start date for searching images. , 
+              #  end_date : str , # End date for searching images.
                ):
+    
     self.crs = crs
-    self.gdf = utils.read_gdf(geometry_path=path_to_geometry,crs=self.crs)
+    self.fc = utils.gdf_to_featureCollection(
+      utils.read_gdf(geometry_path=path_to_geometry,crs=self.crs))
+    # self.start_date=start_date
+    # self.end_date=end_date
+    
+    
+    self.fc = utils.gdf_to_featureCollection(utils.read_gdf
+                                        (geometry_path=path_to_geometry,crs=self.crs))
+
+
+
+
+
+    
 
     
