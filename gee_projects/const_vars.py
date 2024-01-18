75b9b0e0915206cd8c915550ff4dce135801d8f7
reutkeller <reutkeller@gmail.com>
2024-01-18 10:58:44 +0200

    Ready to connect cloud mask script on imge

--- gee_projects/const_vars.py
+++ gee_projects/const_vars.py
@@ -3,7 +3,9 @@
 # %% auto 0
 __all__ = ['CLOUD_FILTER', 'CLD_PRB_THRESH', 'NIR_DRK_THRESH', 'CLD_PRJ_DIST', 'BUFFER', 'CLOUDY_PIXEL_PERCENTAGE_STR',
            'COPERNICUS_S2_CLOUD_PROBABILITY_STR', 's2_img_collection_str', 's2cloudless_str', 'probability_str',
-           'clouds_name_bands_str']
+           'clouds_name_bands_str', 'SCL_str', 'SCL_VALUE_int', 'SR_BAND_SCALE_int', 'B8_str', 'dark_pixels_str',
+           'direction_int', 'MEAN_SOLAR_AZIMUTH_ANGLE_str', 'int_10', 'cloud_transform_str', 'distance_str',
+           'scale_str', 'scale_str_size']
 
 # %% ../nbs/utilities/CONSTANTS.ipynb 3
 import geopandas as gpd
@@ -23,7 +25,24 @@ COPERNICUS_S2_CLOUD_PROBABILITY_STR='COPERNICUS/S2_CLOUD_PROBABILITY'
 # %% ../nbs/utilities/CONSTANTS.ipynb 6
 #S2 image collection
 s2_img_collection_str='COPERNICUS/S2_SR_HARMONIZED'
+
+#clouds bands names
 s2cloudless_str='s2cloudless'
 probability_str='probability'
 clouds_name_bands_str='clouds'
+
+
+#shadow bands names
+SCL_str= 'SCL'
+SCL_VALUE_int = 6
+SR_BAND_SCALE_int = 1e4
+B8_str = 'B8'
+dark_pixels_str='dark_pixels'
+direction_int = 90
+MEAN_SOLAR_AZIMUTH_ANGLE_str='MEAN_SOLAR_AZIMUTH_ANGLE'
+int_10 = 10
+cloud_transform_str='cloud_transform'
+distance_str = 'distance'
+scale_str= 'scale'
+scale_str_size = 100
 
