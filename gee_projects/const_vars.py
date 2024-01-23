edc8dfb41de2e1fb47a033f33b2be2336e3b3a8b
reutkeller <reutkeller@gmail.com>
2024-01-23 19:12:52 +0200

    ready to test version of reduce regions for time series

--- gee_projects/const_vars.py
+++ gee_projects/const_vars.py
@@ -6,11 +6,16 @@ __all__ = ['CLOUD_FILTER', 'CLD_PRB_THRESH', 'NIR_DRK_THRESH', 'CLD_PRJ_DIST', '
            'clouds_name_bands_str', 'SCL_str', 'SCL_VALUE_int', 'SR_BAND_SCALE_int', 'B8_str', 'dark_pixels_str',
            'direction_int', 'MEAN_SOLAR_AZIMUTH_ANGLE_str', 'int_10', 'cloud_transform_str', 'distance_str',
            'scale_str', 'scale_str_size', 'shadows_str', 'crs_str', 'scale_2_int', 'cloudmask_str', 'find_band_str',
-           'system_time_start_str', 'date_format_1', 'date_format_2', 'date_str']
+           'system_time_start_str', 'date_format_1', 'date_format_2', 'date_str', 'MEAN_reducer', 'MEDIAN_reducer',
+           'MAX_reducer', 'MIN_reducer', 'MODE_reducer', 'max_pixels_float', 'get_reducer_with_names']
 
 # %% ../nbs/utilities/CONSTANTS.ipynb 3
 import geopandas as gpd
 import seaborn as sns
+import ee
+import geemap 
+ee.Authenticate()
+ee.Initialize(project='ee-reutkeller')
 
 
 # %% ../nbs/utilities/CONSTANTS.ipynb 5
@@ -58,3 +63,38 @@ system_time_start_str ='system:time_start'
 date_format_1 = 'YYYY-MM-dd'
 date_format_2 = '%Y-%m-%d'
 date_str='date'
+
+# %% ../nbs/utilities/CONSTANTS.ipynb 9
+def get_reducer_with_names( reducer : str ):
+  if reducer == 'mean':
+    ee_reducer = ee.Reducer.mean()
+
+  elif reducer == 'median':
+    ee_reducer = ee.Reducer.median()
+
+  elif reducer == 'min':
+    ee_reducer = ee.Reducer.min()
+
+  elif reducer == 'max' :
+    ee_reducer == ee.Reducer.max()
+
+  elif reducer == 'mode':
+    ee_reducer == ee.Reducer.mode()
+
+  return reducer, ee_reducer 
+
+
+
+
+
+
+
+MEAN_reducer = ee.Reducer.mean()
+MEDIAN_reducer = ee.Reducer.median()
+MAX_reducer = ee.Reducer.max()
+MIN_reducer = ee.Reducer.min()
+MODE_reducer = ee.Reducer.mode()
+
+
+
+max_pixels_float = 1e13
